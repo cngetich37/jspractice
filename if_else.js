c09ba0855049e91ce88e33d5c7a73fa2f5a0ec60
cngetich37 <cngetich37@gmail.com>
2023-07-23 10:24:48 +0300

    added a ifelse and ternary example

---
+++ if_else.js
@@ -0,0 +1,11 @@
+let my_nationality ='Kenya';
+
+if (my_nationality == 'Kenya'){ 
+
+    console.log('Thanks');
+}
+
+else {
+
+    console.log('You are not a Kenyan');
+}
---
+++ ternary_example.js
@@ -0,0 +1,8 @@
+// Determine a number if it is an even or an odd number
+let my_number = 10
+
+ans_wer = my_number%2 === 0 ? "even" : "odd";
+
+console.log(ans_wer);
+
+
